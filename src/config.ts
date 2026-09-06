042b317f4e326c4785da2b5c86b3cb73779347ac
Mohd Rumman Khan <mohdrummankhan96@gmail.com>
2026-09-06 23:22:32 +0530

    update WS_BASE_URL

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL = "https://backend-youtube-watch.onrender.com";
-export const WS_BASE_URL = "https://backend-youtube-watch.onrender.com";
+export const WS_BASE_URL = "wss://backend-youtube-watch.onrender.com";
